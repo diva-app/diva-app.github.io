97c78cf509516f178de8f25324cd8b708159c4cc
githubmor <pc2man@gmail.com>
2022-02-07 17:54:42 +0330

    sdfsf

--- psd.js
+++ psd.js
@@ -4,12 +4,13 @@ var synth = window.speechSynthesis;
 
 var inputTxt = document.querySelector('#text_erea');
 var button = document.querySelector("#btn");
-var result = document.querySelector('#result');
+var resultTxt = document.querySelector('#result');
 
 if (button) {
     button.addEventListener('click', function(){
         $p.spr.load();
         $p.spr.startSpeachRecognation();
+        resultTxt.textContent = $p.spr.result;
     });
 }
 var recognition = new webkitSpeechRecognition();
@@ -27,6 +28,7 @@ load:function($elm){
        inputTxt.textContent = event.results[i][0].transcript;
       if (event.results[i].isFinal) {
         inputTxt.textContent = event.results[i][0].transcript;
+        this.result = event.results[i][0].transcript;
         stopSpeachRecognation();
       }
      }
@@ -39,6 +41,6 @@ load:function($elm){
 },
 stopSpeachRecognation:function(){
     recognition.stop();
-}
+},result:""
 
 }
