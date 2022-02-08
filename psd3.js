665e5b8c1e5a7b58793ebbcb397473a3331ead1c
githubmor <pc2man@gmail.com>
2022-02-08 12:27:55 +0330

    dfgd

---
+++ psd3.js
@@ -0,0 +1,75 @@
+var $p ={};
+
+var synth = window.speechSynthesis;
+//create speech recognition object
+var recognition = new webkitSpeechRecognition();
+
+var resultText = document.querySelector('.text_area');
+var button = document.querySelector(".mic");
+
+//queryselector type of checkbox input
+var checkbox = document.querySelector("input[type='checkbox']");
+//is checkbox checked change recognition language
+checkbox.addEventListener('change', function(){
+    if(checkbox.checked){
+        $p.spr.changeLanguage('en-US');
+    }else{
+        $p.spr.changeLanguage('fa-IR');
+    }
+    resultText.textContent = recognition.lang;
+});
+
+//if button exists, add click event for start recognition
+if (button) {
+    button.addEventListener('click', function(){
+        $p.spr.load();
+        $p.spr.startSpeachRecognation();
+        resultText.textContent = $p.spr.result;
+    });
+}
+
+
+
+$p.spr = {
+    //load speech recognition
+    load:function($elm){
+        //set recognition language to parameter or default to English
+        recognition.lang = $elm.attr('data-lang') || 'en-US';
+        //set continuous mode to true
+        recognition.continuous = true;
+        //set interim results to true
+        recognition.interimResults = true;
+        //add event listener for result
+        recognition.onresult = function (event) {
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    this.result = event.results[i][0].transcript;
+                    stopSpeachRecognation();
+                }
+            }
+        }
+    },
+    startSpeachRecognation:function(){
+        //start recognition
+        recognition.start();
+        //set result to listening
+        this.result = 'Listening .... ';
+        
+    },
+    stopSpeachRecognation:function(){
+        //stop recognition
+        recognition.stop();
+    },
+    //result is the final result of the recognition
+    result:""
+    //change recognition language function
+    ,changeLanguage:function(lang){
+        recognition.lang = lang;
+    }
+
+};
+
+
+ 
+
+
